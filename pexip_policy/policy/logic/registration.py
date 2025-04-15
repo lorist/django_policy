873cdcf34c8318c40e1e9dadf0173e128fd6868f
lorist <dennis.lorist@gmail.com>
2025-04-15 23:21:04 +1000

    add more reg logging to db

--- pexip_policy/policy/logic/registration.py
+++ pexip_policy/policy/logic/registration.py
@@ -12,6 +12,9 @@ from urllib.parse import unquote
 def get_registration_policy(alias_encoded: str, params: dict):
     # decoded_alias = unquote(alias_encoded).lower()
     decoded_alias = unquote(alias_encoded).lower().strip()
+    remote_address = params.get("remote_address", "")
+    protocol = params.get("protocol", "")
+    location = params.get("location", "")
     logger.debug("Registration request for alias: %s", decoded_alias)
 
     allowed_domains = [d.domain for d in AllowedDomain.objects.all()]
@@ -25,7 +28,11 @@ def get_registration_policy(alias_encoded: str, params: dict):
         policy_type="registration",
         alias=decoded_alias,
         action=action,
-        details=params
+        details={
+            "remote_address": remote_address,
+            "protocol": protocol,
+            "location": location
+        }
     )
 
     return {
